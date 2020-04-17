44489f41112d3eabe125d63144a207f22fde7fba
punpat2000 <saint12302@gmail.com>
2020-04-17 23:23:24 +0700

    changing styles with renderer

--- src/app/core/directives/dimension.directive.ts
+++ src/app/core/directives/dimension.directive.ts
@@ -1,15 +1,15 @@
-import { Directive, Input, ElementRef, AfterViewInit } from '@angular/core';
+import { Directive, Input, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appDimension]',
 })
 export class DimensionDirective implements AfterViewInit {
-  @Input() height: number;
-  @Input() width: number;
-  constructor(private el: ElementRef) {}
+  @Input() height!: number;
+  @Input() width!: number;
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
   ngAfterViewInit() {
-    const style = this.el.nativeElement.children[0].style;
-    style.height = `${this.height}px`;
-    style.width = `${this.width}px`;
+    const element: ElementRef = this.el.nativeElement.children[0];
+    this.renderer.setStyle(element, 'height', `${this.height}px`);
+    this.renderer.setStyle(element, 'width', `${this.width}px`);
   }
 }
